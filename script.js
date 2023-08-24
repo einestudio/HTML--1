3a32b9ad8f700d4118b5576a87c212f127baac24
Kenneth Bolo <kebo0001@stud.kea.dk>
2023-08-24 10:45:54 +0200

    .js-5chancces

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+// // const info = document.querySelector("info");
+// console.log("git fucked");
 // const afsnit = document.querySelector(".console");
-// const afsnit1 = document.querySelector(".console1");
-// console.log(afsnit.textContent);
-// afsnit.textContent = "udsolgt";
-// afsnit1.textContent = "Nu er det torsdag";
-// info.innerHTML = "noget nyt";
-// // const afsnit = document.querySelector("header");
+// info.textContent = "noget nyt";
 
-// let overskrift = document.createElement("h2");
-// let text = document.createTextnode("velkommen");
-// overskrift.appendChild("text");
-// // header.appendChild("overskrift");
+console.log("git fucked");
+const afsnit = document.querySelector(".console");
+const afsnit1 = document.querySelector(".console1");
+const afsnit2 = document.querySelector("header");
+const afsnit3 = document.querySelector(".article1");
+const afsnit4 = document.querySelector(".article2");
+console.log(afsnit.textContent);
+afsnit.textContent = "udsolgt";
+afsnit1.textContent = "Nu er det torsdag";
+afsnit2.textContent = "Nu er det torsdag";
+afsnit3.textContent = "gut ficked";
+afsnit4.textContent = "git fucked";
+info.innerHTML = "noget nyt";
+// const afsnit = document.querySelector("header");
+
+let overskrift = document.createElement("h2");
+let text = document.createTextnode("velkommen");
+overskrift.appendChild("text");
+// header.appendChild("overskrift");
