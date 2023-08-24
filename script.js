1684cbce70b1697047c6b2a287908b7461dbd8fb
Kenneth Bolo <kebo0001@stud.kea.dk>
2023-08-24 10:19:45 +0200

    afsnit

---
+++ script.js
@@ -0,0 +1,12 @@
+const afsnit = document.querySelector(".console");
+const afsnit1 = document.querySelector(".console1");
+console.log(afsnit.textContent);
+afsnit.textContent = "Nu er det torsdag";
+afsnit1.textContent = "Nu er det torsdag";
+info.innerHTML = "noget nyt";
+// const afsnit = document.querySelector("header");
+
+let overskrift = document.createElement("h2");
+let text = document.createTextnode("velkommen");
+overskrift.appendChild("text");
+// header.appendChild("overskrift");
